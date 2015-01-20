847b19421b5a61fd3ca03ffe84e611d308548b01
mavrothal <mavrothal@yahoo.com>
2015-01-20 12:43:34 +0200

    Add a README file

--- make_pet.sh
+++ make_pet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+
 DATE=$(date  +%d%m)
 VER=$(grep VERSION= usr/local/petget/pkg_chooser.sh | cut -f 2 -d '=')
 NAME=ppm_auto-${VER}_${DATE}
@@ -9,9 +9,10 @@ cp -aR usr $NAME/
 cp -a pet.specs $NAME/
 sync
 urxvt -e dir2pet $NAME/
+[ $? -ne 0 ] && rm -rf $NAME
 sync
 rm -f ../$NAME.pet
 mv $NAME.pet ../
 rm -rf $NAME
 rm -f nohup.out
-set +e
+
