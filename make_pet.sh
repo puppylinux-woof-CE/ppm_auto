f2ee238ac5acf58c755321d4360e0cb1b0a855e2
mavrothal <mavrothal@yahoo.com>
2015-01-20 13:37:31 +0200

    Add some checks for pet making

--- make_pet.sh
+++ make_pet.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# Check if we can
+if [ "$(df -T `pwd`/make_pet.sh | awk '{print $2}' | tail -n1 | cut -c 1-3)" != "ext" ]; then
+ yaf-splash -bg orange -timeout 5 -text "You need an ext[2-4] filesystem. Exitig"
+ exit 0
+fi
+
+if [ "$(which dir2pet)" = "" ]; then
+ yaf-splash -bg orange -timeout 5 -text "You need should be running puppylinux. Exitig"
+ exit 0
+fi
 
 DATE=$(date  +%d%m)
 VER=$(grep VERSION= usr/local/petget/pkg_chooser.sh | cut -f 2 -d '=')
