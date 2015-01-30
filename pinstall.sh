fc581f43498593550111b6262dcb793af36d7fdb
mavrothal <mavrothal@yahoo.com>
2015-01-30 18:52:34 +0200

    Reword pinstall.sh message

--- pinstall.sh
+++ pinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 [ ! -f /usr/share/pixmaps/puppy/building_block.svg ] &&\
-Xdialog --yesno "You need to install libstardust from\n http://www.murga-linux.com/puppy/viewtopic.php?p=815876#815876 \nfor the new PPM to run properly.\nDo you want to download this pet now?" 0 0 
+Xdialog --yesno "You need to install libstardust pet\nfor the new PPM to run properly.\n\nPlease uninstall this pet.\nInstall the libstardust pet\n and then install the ppm_auto pet again.\n\nDo you want to download the libstardust now?" 0 0 
 [ $? -eq 0 ] &&\
 defaultbrowser http://www.murga-linux.com/puppy/viewtopic.php?p=815876#815876 &
