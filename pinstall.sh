9e3cd7bc89c00687e3f001e59ff9357377ceea0c
mavrothal <mavrothal@yahoo.com>
2015-02-05 14:32:07 +0200

    Remove old ui_Ziggy script when we install

--- pinstall.sh
+++ pinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-[ ! -f /usr/share/pixmaps/puppy/building_block.svg ] &&\
-Xdialog --yesno "You need to install libstardust pet\nfor the new PPM to run properly.\n\nPlease uninstall this pet.\nInstall the libstardust pet\n and then install the ppm_auto pet again.\n\nDo you want to download the libstardust now?" 0 0 
-[ $? -eq 0 ] &&\
-defaultbrowser http://www.murga-linux.com/puppy/viewtopic.php?p=815876#815876 &
+if [ ! -f /usr/share/pixmaps/puppy/building_block.svg ]; then
+	Xdialog --yesno "You need to install libstardust pet\nfor the new PPM to run properly.\n\nPlease uninstall this pet.\nInstall the libstardust pet\n and then install the ppm_auto pet again.\n\nDo you want to download the libstardust now?" 0 0 
+	[ $? -eq 0 ]
+	defaultbrowser http://www.murga-linux.com/puppy/viewtopic.php?p=815876#815876 &
+else
+	rm -f /usr/local/peteget/ui_Ziggy
+	echo Ziggy > /var/local/petget/ui_choice
+fi
